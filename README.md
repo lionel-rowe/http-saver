e0136c0fbb6f7aed7a09c0be59f0f0e5ca1cfe53
lionel-rowe <lionel.rowe@gmail.com>
2025-02-16 18:17:44 +0800

    Add usage example

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # HTTP Saver [![JSR](https://jsr.io/badges/@li/http-saver)](https://jsr.io/@li/http-saver)
 
 Save API responses for testing.
+
+## Usage
+
+```ts
+const httpSaver = new HttpSaver()
+
+Deno.test('some API', () => {
+    using _ = httpSaver.stubFetch()
+
+    // First time the test runs, the response will be saved under `_fixtures/responses`
+    // Subsequent test runs will use the saved response, without making a network request
+    const res = await fetch('https://api.example.com')
+
+    // ... do something with `res`
+})
+```
--- src/httpSaver.ts
+++ src/httpSaver.ts
@@ -71,6 +71,20 @@ export class HttpSaver {
 
 	/**
 	 * Stubs the global fetch function to cache responses.
+	 *
+	 * @example
+	 * ```ts
+	 * const httpSaver = new HttpSaver()
+	 *
+	 * Deno.test('some API', () => {
+	 * 	using _ = httpSaver.stubFetch()
+	 *
+	 * 	// First time the test runs, the response will be saved under `_fixtures/responses`
+	 * 	// Subsequent test runs will use the saved response, without making a network request
+	 * 	const res = await fetch('https://api.example.com')
+	 *
+	 * 	// ... do something with `res`
+	 * })
 	 */
 	stubFetch(): ReturnType<typeof stub<typeof globalThis, 'fetch'>> {
 		return stub(globalThis, 'fetch', async (input, init) => {
